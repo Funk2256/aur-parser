af35e2659e8b8dc4b859a38c6b8bea1e00b15a07
funk2256 <funk2256@gmail.com>
2024-02-28 23:59:36 +0700

    update aur-links-packages.sh

--- aur-links-packages.sh
+++ aur-links-packages.sh
@@ -11,12 +11,14 @@ do
   aur_link="${aur_one_link}${line}"
   aur_curl_count=$(curl -s $aur_link | grep -A2 '<div id="pkgfiles" class="listing">' | sed '3d' | sed '1d' | tr -d ' ' | sed 's/............//' | sed 's/.\{6\}$//')
   aur_sources_link_curl=$(curl -s $aur_link | grep -A50 '<div id="pkgfiles" class="listing">' | grep '<a href' | sed '/dcelasun/d' | tr -d ' ' | grep -Eo $aur_regex | tr -d '"')
+  aur_sources_link_curl_cgit=$(curl -s https://aur.archlinux.org/packages/visual-studio-code-bin | grep -A50 '<div id="pkgfiles" class="listing">' | grep '<a href' | sed '/dcelasun/d' | tr -d ' ' | grep -Eo 'http.*"' | tr -d '"')
 #  aur_sources_link_curl=$(curl -s $aur_link | grep -A50 '<div id="pkgfiles" class="listing">' | grep '<a href' | sed '/dcelasun/d' | tr -d ' ' | grep -Eo $aur_regex_cgit | tr -d '"')
 #  aur_sources_link_curl=$(curl -s $aur_link | grep -A50 '<div id="pkgfiles" class="listing">' | grep '<a href' | sed '/dcelasun/d' | tr -d ' ' | grep -Eo 'http.*"' | tr -d '"')
   echo "Исходников в пакете ${aur_curl_count}"
   echo $aur_sources_link_curl
   echo $aur_sources_link_curl >> links_packages.txt
-
+  echo $aur_sources_link_curl_cgit
+  echo $aur_sources_link_curl_cgit >> links_packages.txt
 #  if echo $aur_curl_link =~ *'<a href="https://github.com/*'*; then
 #    echo "Github"
 #    echo $aur_curl_link >> links_packages.txt
